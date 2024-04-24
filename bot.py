8db790e606b61f22d2acc0016a50fcaeb4b7d821
Mark <81879524+markisik@users.noreply.github.com>
2024-04-24 12:10:40 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 import telebot
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
-token = '7107794543:AAGoqTAZZcT1ZuMdTMk6gw-8J5-7WRSZ7uU'
+load_dotenv()
 
-API = '8a754a26e67c3b0effe5f7e22853f89c'
+token = os.getenv("token")
+API = os.getenv("API")
 
 bot = telebot.TeleBot(token)
+
+def extract_arg(arg):
+    return arg.split()[1:]
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -17,22 +23,31 @@ def porno(message):
    bot.send_message(message.chat.id, 'А ВОТ НЕТУ ТУТ ПОРНУХИ')
 
 @bot.message_handler(commands=['love'])
-def porno(message):
+def love(message):
    bot.send_message(message.chat.id, 'Секретая команда для моей любимой светы❤️')
 
-@bot.message_handler(content_types=['text'])
-def get_weather(message):
-    city = message.text.strip().lower()
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+@bot.message_handler(commands=['weather'])
+#@bot.message_handler(content_types=['text'])
+def weather(message):
+    city = extract_arg(message.text.strip().lower())
+    #city = message.text.strip().lower()
+    bot.send_message(message.chat.id, 'Проверка...')
+    city1 = ''.join(city)
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city1}&appid={API}&units=metric')
+    print(city1)
     if res.status_code == 200:
      data = json.loads(res.text)
      temp = data["main"]["temp"]
-     bot.reply_to(message, f'Cейчас погода: {temp}')
- 
-     image = 'sunny.png' if temp > 5.0 else 'sun.png'
-     file = open('./' + image, 'rb')
-     bot.send_photo(message.chat.id, file)
-    else:
-       bot.reply_to(message, f'Город указан не верно')
+     wind = data["wind"]["speed"]
+     windstorona = data["wind"]["deg"]
+     tempmin = data["main"]["temp_min"]
+     tempmax = data["main"]["temp_max"]
+     bot.reply_to(message, f'Cейчас температура: {temp}' + "\n" + f'Минимальная температура: {tempmin}' + "\n" + f'Максимальная температура: {tempmax}' + "\n" + f'Скорость ветра: {wind}м\с' + "\n" + f'Направление ветра: {windstorona}')
+     #image = 'sun.png' if temp > 5.0 else 'sunny.png'
+     #file = open('./picters/' + image, 'rb')
+     #bot.send_photo(message.chat.id, file)
+    else:    bot.reply_to(message, f'Город указан не верно')
+
+
 
 bot.polling(non_stop=True)
