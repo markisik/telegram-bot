86f6dfa0b398cf565e395004518bec218cacedf9
Mark <81879524+markisik@users.noreply.github.com>
2025-02-13 19:45:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,22 +14,19 @@ def extract_arg(arg):
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    start = message
-    bot.send_message(message.chat.id, 'Пошла нахер дура ёбанная!')
+    bot.send_message(message.chat.id, 'Привет, рад тебя видеть!')
 
 @bot.message_handler(commands=['porno'])
 def porno(message):
-   porno = message
-   bot.send_message(message.chat.id, 'Пошла нахер дура ёбанная')
+   bot.send_message(message.chat.id, 'А ВОТ НЕТУ ТУТ ПОРНУХИ')
 
 @bot.message_handler(commands=['люблю'])
 def love(message):
-   love = message
-   bot.send_message(message.chat.id, 'Пошла нахер дура ёбанная')
+   bot.send_message(message.chat.id, 'Дарую тебе мою любовь')
    for i in range(10):
-    bot.send_message(message.chat.id, 'Пошла нахер дура ёбанная')
+    bot.send_message(message.chat.id, '❤️')
 
-   bot.send_message(message.chat.id, 'Пошла нахер дура ёбанная')
+   bot.send_message(message.chat.id, 'Секретая команда для моей любимой Ани❤️')
 
 
 @bot.message_handler(commands=['w'])
