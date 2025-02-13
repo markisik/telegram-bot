06852e76e2d512d1a46eb8c5b4daf12257aaeb37
Mark <81879524+markisik@users.noreply.github.com>
2025-02-13 21:03:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ def start(message):
     print(message.from_user.first_name)
     print(message.from_user.last_name)
     print(message.from_user.username)
+    print(message.from_user.text)
 
 @bot.message_handler(commands=['porno'])
 def porno(message):
