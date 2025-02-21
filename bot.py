e8412042b51c26d8aa6369192478a24ad9685d43
Mark <81879524+markisik@users.noreply.github.com>
2025-02-21 11:57:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,14 +7,22 @@ token = '7107794543:AAGoqTAZZcT1ZuMdTMk6gw-8J5-7WRSZ7uU'
 
 API = '8a754a26e67c3b0effe5f7e22853f89c'
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(token)
+
+now = datetime.datetime.now()
+
+if __name__ == '__main__':
+    while True:
+        try:
+            bot.polling(none_stop=True, interval=0)
+        except Exception as e: 
+            print('!!!!!!!Ошибка, продолжение работы!!!!!!!')
 
 def extract_arg(arg):
     return arg.split()[1:]
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    now = datetime.datetime.now()
     print(f'ID user: {message.from_user.id} \nName user: {message.from_user.first_name} {message.from_user.last_name} \nNickname user: {message.from_user.username} \nCommand user: {message.text} \n{now} \n------------')
     bot.send_message(message.chat.id, 'Привет, рад тебя видеть!')
     bot.send_message(message.chat.id, 'Вот что я могу:' + "\n" + '/w - погода')
@@ -22,14 +30,12 @@ def start(message):
 
 @bot.message_handler(commands=['porno'])
 def porno(message):
-   now = datetime.datetime.now()
    print(f'ID user: {message.from_user.id} \nName user: {message.from_user.first_name} {message.from_user.last_name} \nNickname user: {message.from_user.username} \nCommand user: {message.text} \n{now} \n------------')
    bot.send_message(message.chat.id, 'А ВОТ НЕТУ ТУТ ПОРНУХИ')
    
 
 @bot.message_handler(commands=['люблю'])
 def love(message):
-   now = datetime.datetime.now()
    print(f'ID user: {message.from_user.id} \nName user: {message.from_user.first_name} {message.from_user.last_name} \nNickname user: {message.from_user.username} \nCommand user: {message.text} \n{now} \n------------')
    bot.send_message(message.chat.id, 'Дарую тебе мою любовь')
    for i in range(10):
@@ -39,9 +45,8 @@ def love(message):
 
 @bot.message_handler(commands=['w'])
 def weather(message):
-   now = datetime.datetime.now()
-   print(f'ID user: {message.from_user.id} \nName user: {message.from_user.first_name} {message.from_user.last_name} \nNickname user: {message.from_user.username} \nCommand user: {message.text} \n{now} \n------------')
    try:
+    print(f'ID user: {message.from_user.id} \nName user: {message.from_user.first_name} {message.from_user.last_name} \nNickname user: {message.from_user.username} \nCommand user: {message.text} \n{now} \n------------')
     city = message.text
     city = city.replace("/", "", 1)
     city = city.replace("w", "", 1)
@@ -64,9 +69,5 @@ def weather(message):
 
 @bot.message_handler(commands=['stop'])
 def stop(message):
-    now = datetime.datetime.now()
     print(f'ID user: {message.from_user.id} \nName user: {message.from_user.first_name} {message.from_user.last_name} \nNickname user: {message.from_user.username} \nCommand user: {message.text} \n{now} \n------------')
     SystemExit.exit(0)
-
-
-bot.polling(non_stop=True)
